dc8bcb339aefc63914c79b119414cd780519cb1c
hckrtst <hckrtst@gmail.com>
2016-08-06 18:51:02 -0700

    checkpt

--- modules/using_modules.exs
+++ modules/using_modules.exs
@@ -1,3 +1,4 @@
+# basic module usage
 defmodule Times do
    def double(n) do
       n * 2
@@ -9,3 +10,12 @@ end
 
 Times.double(34) |> IO.puts
 Times.quadruple(34) |> IO.puts
+
+# more with modules
+defmodule Factorial do
+  def fact(0), do: 1
+  def fact(n), do: n*fact(n-1) 
+end
+
+
+"Factorial of 5 is #{Factorial.fact(5)}" |> IO.puts
