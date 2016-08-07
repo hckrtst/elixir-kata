c97d8bdf80fe1517a45e7226602c35406e97d0d0
Sanket K <hckrtst@gmail.com>
2016-08-06 23:12:03 -0700

    checkpoint - more functions

---
+++ modules/sum.exs
@@ -0,0 +1,6 @@
+defmodule Math do
+   def sum(1), do: 1
+   def sum(n) do
+      n+sum(n-1)
+   end
+end
--- modules/using_modules.exs
+++ modules/using_modules.exs
@@ -14,8 +14,14 @@ Times.quadruple(34) |> IO.puts
 # more with modules
 defmodule Factorial do
   def fact(0), do: 1
-  def fact(n), do: n*fact(n-1) 
+  def fact(n), do: n*fact(n-1)
 end
 
+# whoa this is fast!
+"Factorial of 5000 is #{Factorial.fact(5000)}" |> IO.puts
 
-"Factorial of 5 is #{Factorial.fact(5)}" |> IO.puts
+# bad factorial - remember to always provide the most specific cases first
+defmodule BadFactorial do
+   def fact(n), do: n*fact(n-1)
+   def fact(0), do: 1
+end
